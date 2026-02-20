e743dfcabb2a6bfa6de3b50fd3980f7e75fdfc0a
tez-ferguson <tez@kinglyclark.com>
2026-02-20 19:59:11 +0200

    Update Next.js build artifacts and routing manifests
    - Removed outdated app path routes manifest and build ID files.
    - Updated app build manifest to reflect new routes and assets for marketing and auth pages.
    - Adjusted references in build manifest for polyfills and main app files.
    
    This commit ensures the application is aligned with the latest routing and asset configurations, improving build efficiency.

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-3b5a19591aa0c84d.js","static/chunks/fd9d1056-0e927b4f5b6d0f6f.js","static/chunks/117-5bd60fbe9a80e34f.js","static/chunks/main-app-f0390f4731db0713.js"],pages:{"/_app":["static/chunks/webpack-3b5a19591aa0c84d.js","static/chunks/framework-00a8ba1a63cfdc9e.js","static/chunks/main-ba5f054f0cdb395b.js","static/chunks/pages/_app-15e2daefa259f0b5.js"],"/_error":["static/chunks/webpack-3b5a19591aa0c84d.js","static/chunks/framework-00a8ba1a63cfdc9e.js","static/chunks/main-ba5f054f0cdb395b.js","static/chunks/pages/_error-28b803cb2479b966.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
